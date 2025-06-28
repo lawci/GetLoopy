29e9f2363658efcd3fdb7a6b672e034f54036435
lawci <lcft1204@gmail.com>
2025-06-28 23:49:18 +0800

    commit

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
